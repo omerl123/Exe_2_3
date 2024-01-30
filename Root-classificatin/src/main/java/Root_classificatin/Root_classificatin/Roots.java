dea8806b2bac432c819b96a7856d8aa74dea3bdf
omerl <omerl@DESKTOP-PNVMPVO.mshome.net>
2024-01-30 16:39:19 +0200

    We changed the range of a, b from 50 to 100

--- Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
+++ Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
@@ -28,7 +28,7 @@ class Roots
       
       q = b*b - 4*a*c;
      
-      if (out_of_Range(a, 0, 50)|| out_of_Range(b, 0, 50)||out_of_Range(c, 0, 100)) {
+      if (out_of_Range(a, 0, 100)|| out_of_Range(b, 0, 100)||out_of_Range(c, 0, 100)) {
     	  roots = Root_Types.ERROR;
     	  return;
       }
